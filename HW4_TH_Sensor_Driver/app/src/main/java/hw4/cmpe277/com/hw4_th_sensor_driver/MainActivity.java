3af41df79ec5cc3e59a8f555f08028976d8b7efd
Stephen Chen <sun999@gmail.com>
2017-03-17 02:00:22 -0700

    HW4

--- HW4_TH_Sensor_Driver/app/src/main/java/hw4/cmpe277/com/hw4_th_sensor_driver/MainActivity.java
+++ HW4_TH_Sensor_Driver/app/src/main/java/hw4/cmpe277/com/hw4_th_sensor_driver/MainActivity.java
@@ -2,12 +2,44 @@ package hw4.cmpe277.com.hw4_th_sensor_driver;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements UIUpdateCallBack {
+    private EditText getTempUI = null;
+    private EditText getHumiUI = null;
+    private EditText getActUI = null;
+    private EditText getReadCountUI = null;
+    private EditText getOutputUI = null;
+    private int readCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getTempUI = (EditText) findViewById(R.id.IdTemperature);
+        getHumiUI = (EditText) findViewById(R.id.IdTemperature);
+        getActUI = (EditText) findViewById(R.id.IdTemperature);
+        getReadCountUI = (EditText) findViewById(R.id.IdTemperature);
+        getOutputUI = (EditText) findViewById(R.id.IdTemperature);
+    }
+
+    @Override
+    public void UpdateUI(int temp, int humi, int actId, int counter) {
+        getTempUI.setText(temp + "");
+        getHumiUI.setText(humi + "");
+        getActUI.setText(actId + "");
+        getReadCountUI.setText(readCount - counter + "");
+        getOutputUI.setText(humi + "");
+    }
+
+    public void Generate(View view) {
+        this.readCount = Integer.parseInt(getReadCountUI.getText().toString());
+    }
+
+    public void Cancel(View view) {
+
     }
 }
---
+++ HW4_TH_Sensor_Driver/app/src/main/java/hw4/cmpe277/com/hw4_th_sensor_driver/UIUpdateCallBack.java
@@ -0,0 +1,9 @@
+package hw4.cmpe277.com.hw4_th_sensor_driver;
+
+/**
+ * Created by Stephen on 3/17/2017.
+ */
+
+public interface UIUpdateCallBack {
+    void UpdateUI(int temp, int humi, int actId, int counter);
+}
